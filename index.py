3fdac131b5912e200bd610459a50ffce9c4eb55b
Maryamshraf <142664253+Maryamshraf@users.noreply.github.com>
2023-08-27 23:24:15 +0300

    7

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ print(f"my name is{my_name} and iam {my_age} years old")
 first_number= int(input("enter first number:"))
 secound_number= int(input("enter secound number:"))
 
-operation = input("enter the operation")
+operation = input("enter the operation:")
 if operation == "+" :
     print(first_number+secound_number)
     
@@ -16,14 +16,15 @@ elif operation== "/":
     print(first_number/secound_number) 
 else: 
     print("the operation is not valid")
- bus_capacity = int(input("what is the bus capacity?"))
-  inbus = int(input("how many people are in the bus? "))
+bus_capacity = int(input("what is the bus capacity?"))
+inbus = int(input("how many people are in the bus? "))
 outbus = int(input("how many people are out of the bus?"))  
 
 
-   
-print("empty_seats")
 empty_seats = bus_capacity - inbus
+   
+print( f"there are {empty_seats}  empty seats in the bus")
+
     
 if empty_seats>outbus:
         print("you can get in")
